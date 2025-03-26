3439b3c7695546ff16450a4b3d954ee2851934eb
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-03-27 02:30:54 +0300

    app build

---
+++ GroceriesApp/GroceriesApp/GroceriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesAppApp.swift
+//  GroceriesApp
+//
+//  Created by Dmitrii Nazarov on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
