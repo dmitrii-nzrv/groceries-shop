7ca3e8a7371126d4917c919e889165e520c93114
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-03-27 03:23:52 +0300

    add MVVM structure + set launch screen + create WelcomeView

---
+++ GroceriesApp/GroceriesApp/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  GroceriesApp
+//
+//  Created by Dmitrii Nazarov on 27.03.2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
