6b2be0791235edd4315f292d98d4060b235b7bd5
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-04-01 12:36:25 +0300

    make and set SignInView

---
+++ GroceriesApp/GroceriesApp/UICommon/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  GroceriesApp
+//
+//  Created by Dmitrii Nazarov on 30.03.2025.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
---
+++ GroceriesApp/GroceriesApp/View/Login/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  GroceriesApp
+//
+//  Created by Dmitrii Nazarov on 31.03.2025.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
