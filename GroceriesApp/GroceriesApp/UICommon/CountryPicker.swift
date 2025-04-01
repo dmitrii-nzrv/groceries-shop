c5ef5358706fb75b763782ac66fadbe0368f74fd
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-04-01 13:11:33 +0300

    add countryPicker+flags

---
+++ GroceriesApp/GroceriesApp/UICommon/CountryPicker.swift
@@ -0,0 +1,18 @@
+//
+//  CountryPicker.swift
+//  GroceriesApp
+//
+//  Created by Dmitrii Nazarov on 01.04.2025.
+//
+
+import SwiftUI
+
+struct CountryPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryPicker()
+}
